d3a99762e59c75a619017660e28e587f07fd6289
ooooo <297872913@qq.com>
2023-08-03 21:08:15 +0800

    crates

---
+++ crates/lib/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn public_function() {
+    println!("called rary's `public_function()`");
+}
+
+fn private_function() {
+    println!("called rary's `private_function()`");
+}
+
+pub fn indirect_access() {
+    print!("called rary's `indirect_access()`, that\n> ");
+
+    private_function();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
