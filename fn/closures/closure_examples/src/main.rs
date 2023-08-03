089dfff0854e966c30ecb8d36542366d3ec1edf9
ooooo <297872913@qq.com>
2023-08-03 10:54:58 +0800

    fn/closures/closure_examples

---
+++ fn/closures/closure_examples/src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+    iter_any();
+    iter_find();
+    iter_position();
+}
+
+
+fn iter_any() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+    println!("2 in vec1: {}", vec2.into_iter().any(|x| x == 2));
+
+    println!("vec1 len: {}", vec1.len());
+    println!("First element of vec1 is: {}", vec1[0]);
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    // `iter()` for arrays yields `&i32`.
+    println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
+    // `into_iter()` for arrays yields `i32`.
+    println!("2 in array2: {}", array2.into_iter().any(|x| x == 2));
+}
+
+fn iter_find() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    println!("Find 2 in vec1: {:?}", vec1.iter().find(|&&x| x == 2));
+    println!("Find 2 in vec2: {:?}", vec2.into_iter().find(|&x| x == 2));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    // `iter()` for arrays yields `&i32`
+    println!("Find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2));
+    // `into_iter()` for arrays yields `i32`
+    println!("Find 2 in array2: {:?}", array2.into_iter().find(|&x| x == 2));
+}
+
+fn iter_position() {
+    let vec = vec![1, 9, 3, 3, 13, 2];
+
+    // `iter()` for vecs yields `&i32` and `position()` does not take a reference, so
+    // we have to destructure `&i32` to `i32`
+    let index_of_first_even_number = vec.iter().position(|&x| x % 2 == 0);
+    assert_eq!(index_of_first_even_number, Some(5));
+
+    // `into_iter()` for vecs yields `i32` and `position()` does not take a reference, so
+    // we do not have to destructure
+    let index_of_first_negative_number = vec.into_iter().position(|x| x < 0);
+    assert_eq!(index_of_first_negative_number, None);
+}
