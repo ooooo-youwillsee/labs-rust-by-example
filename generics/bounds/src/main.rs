e22e93115d734d573ad9d9135de464d7926c6f3b
ooooo <297872913@qq.com>
2023-08-05 11:07:24 +0800

    generics/bounds

---
+++ generics/bounds/src/main.rs
@@ -0,0 +1,49 @@
+use std::fmt::{Debug, Display};
+
+fn printer<T: Display>(t: T) {
+    println!("{}", t)
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: f64,
+    height: f64,
+}
+
+#[allow(dead_code)]
+struct Triangle {
+    length: f64,
+    height: f64,
+}
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 {
+        self.length * self.height
+    }
+}
+
+fn print_debug<T: Debug>(t: T) {
+    println!("{:?}", t)
+}
+
+fn area<T: HasArea>(t: T) -> f64 {
+    t.area()
+}
+
+
+fn main() {
+    let rectangle = Rectangle { length: 3.0, height: 4.0 };
+    let _triangle = Triangle { length: 3.0, height: 4.0 };
+
+    print_debug(rectangle);
+    println!("Area: {}", area(rectangle));
+
+    //print_debug(&_triangle);
+    //println!("Area: {}", area(&_triangle));
+    // ^ TODO: Try uncommenting these.
+    // | Error: Does not implement either `Debug` or `HasArea`.
+}
