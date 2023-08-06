f244778371bb91b3b89189d912d85d87521bb5f5
ooooo <297872913@qq.com>
2023-08-06 18:15:01 +0800

    scope/lifetime/explicit

---
+++ scope/lifetime/explicit/src/main.rs
@@ -0,0 +1,25 @@
+fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("x is {} and y is {}", x, y);
+}
+
+fn failed_borrow<'a>() {
+    let _x = 12;
+    // let y: &'a i32 = &_x;
+}
+
+
+fn main() {
+    // Create variables to be borrowed below.
+    let (four, nine) = (4, 9);
+
+    // Borrows (`&`) of both variables are passed into the function.
+    print_refs(&four, &nine);
+    // Any input which is borrowed must outlive the borrower.
+    // In other words, the lifetime of `four` and `nine` must
+    // be longer than that of `print_refs`.
+
+    failed_borrow();
+    // `failed_borrow` contains no references to force `'a` to be
+    // longer than the lifetime of the function, but `'a` is longer.
+    // Because the lifetime is never constrained, it defaults to `'static`.
+}
